90857966e4274646ec34bf39bf611bbe53bd5c46
Sergey Prohanov <sergey@wmstudio.su>
2026-09-01 11:38:05 +0400

    Bump version to 1.0.1

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.0.0",
-    "VERSION_DATE" => "2026-09-01 10:00:00"
+    "VERSION" => "1.0.1",
+    "VERSION_DATE" => "2026-09-01 11:40:00"
 );
